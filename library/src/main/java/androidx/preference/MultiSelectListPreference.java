cfa95304e01ad5a5f58233a3d5ffbaef66e908d3
arkon <eugcheung94@gmail.com>
2021-05-08 09:45:57 -0400

    Add MultiSelectListPreference

---
+++ library/src/main/java/androidx/preference/MultiSelectListPreference.java
@@ -0,0 +1,40 @@
+package androidx.preference;
+
+import android.content.Context;
+
+import java.util.Set;
+
+public class MultiSelectListPreference extends DialogPreference {
+
+    public MultiSelectListPreference(Context context) {
+        throw new RuntimeException("Stub!");
+    }
+
+    public void setEntries(CharSequence[] entries) {
+        throw new RuntimeException("Stub!");
+    }
+
+    public CharSequence[] getEntries() {
+        throw new RuntimeException("Stub!");
+    }
+
+    public void setEntryValues(CharSequence[] entryValues) {
+        throw new RuntimeException("Stub!");
+    }
+
+    public CharSequence[] getEntryValues() {
+        throw new RuntimeException("Stub!");
+    }
+
+    public void setValues(Set<String> values) {
+        throw new RuntimeException("Stub!");
+    }
+
+    public Set<String> getValues() {
+        throw new RuntimeException("Stub!");
+    }
+
+    public int findIndexOfValue(String value) {
+        throw new RuntimeException("Stub!");
+    }
+}
