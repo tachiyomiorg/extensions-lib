6f52cd9f99b3a3bf25ec947147ac324f9202ef10
arkon <eugcheung94@gmail.com>
2022-02-01 12:55:10 -0500

    Add new manga statuses

--- library/src/main/java/eu/kanade/tachiyomi/source/model/SManga.kt
+++ library/src/main/java/eu/kanade/tachiyomi/source/model/SManga.kt
@@ -25,6 +25,9 @@ interface SManga {
         const val ONGOING = 1
         const val COMPLETED = 2
         const val LICENSED = 3
+        const val PUBLISHING_FINISHED = 4
+        const val CANCELLED = 5
+        const val ON_HIATUS = 6
 
         fun create(): SManga {
             throw Exception("Stub!")
