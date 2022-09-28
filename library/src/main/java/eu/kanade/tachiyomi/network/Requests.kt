fd8c3f5ab0bfe078c5caac0be2c9d6a1683e54ba
stevenyomi <95685115+stevenyomi@users.noreply.github.com>
2022-09-28 15:11:33 +0800

    Requests: add GET(HttpUrl) overload

--- library/src/main/java/eu/kanade/tachiyomi/network/Requests.kt
+++ library/src/main/java/eu/kanade/tachiyomi/network/Requests.kt
@@ -2,6 +2,7 @@ package eu.kanade.tachiyomi.network
 
 import okhttp3.CacheControl
 import okhttp3.Headers
+import okhttp3.HttpUrl
 import okhttp3.Request
 import okhttp3.RequestBody
 
@@ -10,6 +11,16 @@ private val DEFAULT_HEADERS: Headers = throw Exception("Stub!")
 private val DEFAULT_BODY: RequestBody = throw Exception("Stub!")
 
 fun GET(url: String,
+        headers: Headers = DEFAULT_HEADERS,
+        cache: CacheControl = DEFAULT_CACHE_CONTROL): Request {
+
+    throw Exception("Stub!")
+}
+
+/**
+ * @since extensions-lib 1.4
+ */
+fun GET(url: HttpUrl,
         headers: Headers = DEFAULT_HEADERS,
         cache: CacheControl = DEFAULT_CACHE_CONTROL): Request {
 
