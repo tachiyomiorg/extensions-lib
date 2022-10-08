71fe20d3b8f8e86c396e5f0cdb2387fc01204024
arkon <arkon@users.noreply.github.com>
2022-10-08 09:45:10 -0400

    Add JavaScriptEngine abstraction to extensions-lib (#9)

---
+++ library/src/main/java/eu/kanade/tachiyomi/network/JavaScriptEngine.kt
@@ -0,0 +1,19 @@
+package eu.kanade.tachiyomi.network
+
+import android.content.Context
+
+/**
+ * Util for evaluating JavaScript in sources.
+ */
+class JavaScriptEngine(context: Context) {
+
+    /**
+     * Evaluate arbitrary JavaScript code and get the result as a primtive type
+     * (e.g., String, Int).
+     *
+     * @since extensions-lib 1.4
+     * @param script JavaScript to execute.
+     * @return Result of JavaScript code as a primitive type.
+     */
+    suspend fun <T> evaluate(script: String): T = throw Exception("Stub!")
+}
