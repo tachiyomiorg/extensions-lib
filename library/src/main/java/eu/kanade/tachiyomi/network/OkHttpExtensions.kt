b107ef035ec5403f675cdad022e1fc50d135d272
Alessandro Jean <14254807+alessandrojean@users.noreply.github.com>
2023-08-18 23:07:06 -0300

    Share HttpException from the app with the extensions-lib. (#12)

--- library/src/main/java/eu/kanade/tachiyomi/network/OkHttpExtensions.kt
+++ library/src/main/java/eu/kanade/tachiyomi/network/OkHttpExtensions.kt
@@ -9,3 +9,12 @@ fun Call.asObservable(): Observable<Response> = throw Exception("Stub!")
 fun Call.asObservableSuccess(): Observable<Response> = throw Exception("Stub!")
 
 suspend fun Call.await(): Response = throw Exception("Stub!")
+
+/**
+ * Exception that handles HTTP codes considered not successful by OkHttp.
+ * Use it to have a standardized error message in the app across the extensions.
+ *
+ * @since extensions-lib 1.5
+ * @param code [Int] the HTTP status code
+ */
+class HttpException(val code: Int) : IllegalStateException("HTTP error $code")
